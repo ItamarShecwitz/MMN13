1c7ee1e30f4a80fc6dea77d7b36eb9a94ede4b49
unknown <thewaterpaper11@gmail.com>
2024-06-30 12:23:56 +0300

    improved the skeleton

---
+++ MergableHeap.java
@@ -0,0 +1,27 @@
+public class MergableHeap {
+    private int[] heap;
+    
+    public MergableHeap() {
+        this.heap = new int[1];
+    }
+
+    public static MergableHeap make_heap() {
+        return null;
+    }
+
+    public void insert(MergableHeap heap) {
+        return;
+    }
+
+    public void minimum(MergableHeap heap) {
+        return;
+    }
+
+    public void extract_min(MergableHeap heap) {
+        return;
+    }
+
+    public void union(MergableHeap heap) {
+        return;
+    }
+}
