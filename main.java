1c088e9e22b3dbfcb7d52d3654a55e1682e5e622
unknown <thewaterpaper11@gmail.com>
2024-07-01 01:02:05 +0300

    more clear example

--- main.java
+++ main.java
@@ -1,9 +1,18 @@
 class Main {
     public static void main(String[] args){
+        System.out.println("\nInitilized new heap from file named heaper:");
         MergableHeap heaper = MergableHeap.make_heap_from_file("./example.txt");
+
+        System.out.println("\nheaper:");
         heaper.print_heap();
+
+        System.out.println("\nheaper min value:");
         System.out.println(heaper.extract_min());
+
+        System.out.println("\nheaper without min value:");
         heaper.print_heap();
+
+
         heaper.insert(17);
         heaper.insert(2);
         heaper.insert(5);
@@ -12,7 +21,11 @@ class Main {
         heaper.insert(120);
         heaper.insert(4);
         heaper.insert(1);
+
+        System.out.println("\nheaper after adding 8 new nodes:");
         heaper.print_heap();
+
+        System.out.println("\nheaper min value (again):");
         System.out.println(heaper.minimum());
 
         MergableHeap heaper2 = MergableHeap.make_heap();
@@ -24,12 +37,20 @@ class Main {
         heaper2.insert(1210);
         heaper2.insert(14);
         heaper2.insert(143);
+
+        System.out.println("\nmaking heaper2, with the make_heap function this time:");
         heaper2.print_heap();
 
+        System.out.println("\nunite the heapers:");
         MergableHeap new_heap = MergableHeap.union(heaper, heaper2);
         new_heap.print_heap();
 
+        System.out.println("\nnew united heaper min value:");
         System.out.println(new_heap.extract_min());
+
+        
+        System.out.println("\nnew heaper without min value:");
         new_heap.print_heap();
+        
     }
 }
